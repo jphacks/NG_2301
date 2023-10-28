bd9913de4ed3994d7dc8e824002fd157b9cf5e41
ibukani <ibukani2056@gmail.com>
2023-10-28 14:03:43 +0900

    地点登録のページと機能を実装

---
+++ src/components/RegisterLocation.jsx
@@ -0,0 +1,59 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../Context/AuthContext";
+import { useForm } from "react-hook-form";
+import { addLocation } from "../firebase/database";
+import { useState } from "react";
+
+const RegisterLocation = () => {
+  const [location, setLocation] = useState("");
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleChangeLocation = (event) => {
+    setLocation(event.currentTarget.value);
+  };
+  const onSubmit = () => {
+    console.log(user.uid);
+    addLocation(user.uid, location);
+    navigate("Home");
+    reset();
+  };
+
+  if (!user) {
+    return <Navigate to="/signin" />;
+  } else {
+    return (
+      <div>
+        <h1>地点登録</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>登録地点 (住所可)</label>
+            <input
+              {...register("location", {
+                required: {
+                  value: true,
+                  message: "登録したい地点を入力してください",
+                },
+              })}
+              name="location"
+              placeholder="location"
+              onChange={(event) => handleChangeLocation(event)}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <div>
+            <button type="submit">登録</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export { RegisterLocation };
