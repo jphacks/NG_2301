d64bda0bf45850ae30e60ed8c30669c66cce55fe
Aoyama Kazuki <sekibun3109@gmail.com>
2023-10-28 14:05:38 +0900

    気圧データのjson作成

--- src/APIfunction/weatherAPI.js
+++ src/APIfunction/weatherAPI.js
@@ -1,35 +1,33 @@
 /*
 WeatherAPIをたたいてログ出力する関数
 */
-
-const API_KEY = import.meta.env.API_KEY;
-
-
 async function fetchWeatherData() {
+  // 天気予報データの定義
+  const location = "Nagoya";
+  const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
 
-    // 天気予報データの定義
-    const location = 'Nagoya';
-    //const API_KEY = PropertiesService.getScriptProperties().getProperty("API_KEY");
+  // 天気予報データのURL
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${API_KEY}`;
 
-    // 天気予報データのURL
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${API_KEY}`;
+  let weatherData = [];
 
-    // URLを使ってAPIからデータを取得
-    const response = await fetch(url);
-    console.log(response); 
-    const jsondata = response.getContentText();
-    
-    // JSONデータをパース
-    const data = JSON.parse(jsondata);
-
-    Logger.log(jsondata);
-
-    // タイムスタンプを変換
-    for (let i = 0; i < data.list.length; i++) {
-        const jst = new Date(data.list[i].dt * 1000).toLocaleString("ja-JP", {timeZone: "Asia/Tokyo"});
-        const pressure = data.list[i].main.pressure;
-        console.log(jst,pressure);
-    }
+  // URLを使ってAPIからデータを取得
+  await fetch(url, {
+    method: "get",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.list.length; i++) {
+        if (i % 2 === 0) {
+          const jst = new Date(data.list[i].dt * 1000).toLocaleString("ja-JP", {
+            timeZone: "Asia/Tokyo",
+          });
+          const pressure = data.list[i].main.pressure;
+          weatherData.push({ jst, pressure });
+        }
+      }
+    });
+  console.log(weatherData);
 }
 
-export {fetchWeatherData}
+export { fetchWeatherData };
