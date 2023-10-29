40beeee66b8268d2e8c3cc95549187a5f2889a6c
Aoyama Kazuki <sekibun3109@gmail.com>
2023-10-29 10:35:43 +0900

    ホームと登録画面の修正

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,8 +71,6 @@ const Home = () => {
     if (pressureNumData == undefined) {
       return;
     }
-    const num = pressureNumData[0];
-
     return (
       <tr key={location.id}>
         <td key={location.id}>{location.location}</td>
@@ -94,12 +92,6 @@ const Home = () => {
   } else {
     return (
       <div>
-        <h1>ホームページ</h1>
-        <p>ユーザーそれぞれでurl変えないといけない</p>
-        <p>登録地点</p>
-        {locations.map((location) => (
-          <p key={location.id}>{location.location}</p>
-        ))}
         <table>
           <thead>
             <tr>
